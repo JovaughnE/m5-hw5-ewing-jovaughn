23d0ae18f3d0000f5f1bec8f0b83c5d5b290ec6b
Jovaughn E <jovaughn.ewing@ufl.edu>
2021-02-10 23:02:11 -0700

    Initial commit and added JS code.

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-var pizza = Object ();
-Object.crust = "10";
-Object.cheese = "12";
+let pizza = new Object();
+pizza.crust = "plain";
+pizza.cheese = "mozzarella";
+pizza.sauce = "red";
+pizza.addToppings = addToppings() {
+    console.log("Please add pepperoni and sausage");
+}
+pizza.addToppings(); 
 
-addToppings 
+pizza.orderPizza = orderPizza() {
+    console.log(crust);
+    console.log(cheese);
+    console.log(sauce);
+    console.log(addToppings(););
+}
